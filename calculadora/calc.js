dce27c234db79612f6f62efe99fe579d399db2cb
De Souza <ramon.desouza@thomsonreuters.com>
2020-10-27 12:28:33 -0300

    27-10-12:20

---
+++ calculadora/calc.js
@@ -0,0 +1,9 @@
+var somaFunc = require("./soma")
+var subFunc  = require("./sub")
+var divFunc  = require("./div")
+var multFunc = require("./mult")
+
+console.log(somaFunc(3,2))
+console.log(subFunc(3,2))
+console.log(divFunc(3,2))
+console.log(multFunc(3,2))
